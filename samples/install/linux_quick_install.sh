80fa05057f1d05fa7da7d69edd8a42b4f924e7ed
cabelo <cabelo@opensuse.org>
2020-08-28 02:22:38 -0300

    Other Linux

--- samples/install/linux_quick_install.sh
+++ samples/install/linux_quick_install.sh
@@ -8,8 +8,17 @@ sudo()
 fi
 
 # [body]
-# Install minimal prerequisites (Ubuntu 18.04 as reference)
+# Install minimal prerequisites in Ubuntu  Ubuntu 10.04, 16.04 LST and 18.04 LST
 sudo apt update && sudo apt install -y cmake g++ wget unzip
+
+# Install minimal prerequisites in openSUSE Leap 15.2, opensSUSE Tumbleweed and SUSE Linux Enterprise Server
+sudo zypper in patterns-devel-C-C++-devel_C_C++
+
+# Install minimal prerequisites in openSUSE Leap 15.2, opensSUSE Tumbleweed and SUSE Linux Enterprise Server
+sudo zypper in patterns-devel-C-C++-devel_C_C++ unzip  wget 
+
+# Install minimal prerequisites in CentOS 8 or Red Hat Enterprise Linux 8
+sudo dnf -y install gcc gcc-c++ cmake wget unzip
 
 # Download and unpack sources
 wget -O opencv.zip https://github.com/opencv/opencv/archive/master.zip
