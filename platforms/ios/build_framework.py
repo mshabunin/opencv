33a12e044d0b52b4ae97da31421a1ad8e2c8a384
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-10-23 15:04:49 +0300

    hack for ios

--- platforms/ios/build_framework.py
+++ platforms/ios/build_framework.py
@@ -44,7 +44,7 @@ else:
 sys.path.insert(0, os.path.abspath(os.path.abspath(os.path.dirname(__file__))+'/../apple'))
 from cv_build_utils import execute, print_error, get_xcode_major, get_xcode_setting, get_xcode_version, get_cmake_version
 
-IPHONEOS_DEPLOYMENT_TARGET='10.0'  # default, can be changed via command line options or environment variable
+IPHONEOS_DEPLOYMENT_TARGET='11.0'  # default, can be changed via command line options or environment variable
 
 CURRENT_FILE_DIR = os.path.dirname(__file__)
 
@@ -286,7 +286,8 @@ class Builder:
                 cmakecmd.append("-DCPU_BASELINE=DETECT")
                 cmakecmd.append("-DCMAKE_CROSSCOMPILING=ON")
                 cmakecmd.append("-DOPENCV_WORKAROUND_CMAKE_20989=ON")
-
+        if target.lower() == "iphone" and arch == "armv7s":
+            cmakecmd.append("-DWITH_PROTOBUF=OFF")
         cmakecmd.append(dir)
         cmakecmd.extend(cmakeargs)
         return cmakecmd
