ac9d3ad3acf412987ed1c712088f09ccec6335da
Maksim Shabunin <maksim.shabunin@gmail.com>
2024-10-23 14:49:20 +0300

    move msvc warnings workaround to the header

--- modules/core/src/hal_internal.cpp
+++ modules/core/src/hal_internal.cpp
@@ -47,6 +47,11 @@
 
 #ifdef HAVE_LAPACK
 
+#ifdef _MSC_VER
+#  define _SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING
+#  define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+#endif
+
 #include <complex.h>
 #include "opencv_lapack.h"
 
